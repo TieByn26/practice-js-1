42d1e39cbf3f86494f530467731388d6476ad55e
TieByn26 <dxuantienn@gmail.com>
2024-08-20 17:42:37 +0700

    create basic routes

---
+++ src/constants/routes.js
@@ -0,0 +1,17 @@
+import {menu, home, order, product, seller, customer, analytics, categories} from "@/view";
+
+export const routesPath = {
+    home: "/",
+    order: "/order",
+    product: "/product",
+    seller: "/seller",
+    categories: "categories",
+    menu: "/menu",
+    customer : "/customer",
+    analytics : "/analytics"
+};
+
+export const routeComponents = {
+
+  };
+        
