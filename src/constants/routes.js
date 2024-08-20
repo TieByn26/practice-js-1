cafe8ff41331c7f2d514f4d11b3d3a638078539f
TieByn26 <dxuantienn@gmail.com>
2024-08-20 19:11:47 +0700

    continue routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -12,6 +12,13 @@ export const routesPath = {
 };
 
 export const routeComponents = {
-
+    [routesPath.home]: new home().render(),
+    [routesPath.product]: new product().render(),
+    [routesPath.order]: new order().render(),
+    [routesPath.menu]: new menu().render(),
+    [routesPath.categories]: new categories().render(),
+    [routesPath.seller]: new seller().render(),
+    [routesPath.customer]: new customer().render(),
+    [routesPath.analytics]: new analytics().render()
   };
         
