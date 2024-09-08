589c1ba278fecd6f31e8781b833fa2e9fa150719
TieByn26 <dxuantienn@gmail.com>
2024-09-08 20:11:41 +0700

    refactor structure #1

---
+++ src/view/components/elements/index.js
@@ -0,0 +1,4 @@
+export * from "./link/index";
+export * from "./list/index";
+export * from "./button/index";
+export * from "./chevron/index";
---
+++ src/view/components/layouts/header.js
@@ -0,0 +1,19 @@
+import { NavHeader } from "../elements";
+export class header {
+    constructor(){
+
+    }
+    headerLayout(){
+        const header = document.createElement("header");
+        const nav = document.createElement("nav");
+        nav.className = "nav-header";
+        nav.innerHTML = new NavHeader().render();
+        header.appendChild(nav);
+
+        return header;
+    }
+
+    render(){
+        return this.headerLayout().outerHTML;
+    }
+}
