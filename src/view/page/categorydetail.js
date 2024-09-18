addcfcd3cb6834674c565bbba7ae63239924f9e4
TieByn26 <dxuantienn@gmail.com>
2024-09-18 22:30:29 +0700

    new page

---
+++ src/view/page/categorydetail.js
@@ -0,0 +1,13 @@
+export class categorydetail {
+    constructor() {
+        
+    }
+    mainMethod(){
+        return /*html*/`
+            <div> categorydetail </div>
+        `;
+    }
+    render(){
+        return this.mainMethod();
+    }
+}
