c3b1666ef07a6d2a0139dd3081e7eff6c34c3676
TieByn26 <dxuantienn@gmail.com>
2024-09-22 11:14:16 +0700

    display data #1

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,30 +1,47 @@
 import { endpointUrl } from "@/utils";
 import { axiosApiGetData, axiosApiDeleteData, axiosApiUpdateData, axiosApiAddData } from "@/utils";
-import { product, category, image} from "@/models";
-export class productController{
-    constructor(params) {
-        
-    }
+import { product, category } from "@/models";
+
+export class productController {
+    constructor(params) {}
+
     /**
      * 
-     * @param {number} page
-     * @return {Array} product[]
+     * @param {pageNumber} page 
+     * @param {callback} responseData 
      */
-    getDataFlPage(page, responseData = (product) => {
-        return product;
-    }){
-        axiosApiGetData(endpointUrl.getTopSelling(page))
-        .then(data => {
-            /**@type {product[]}*/
+    async getDataFlPage(page, responseData = (products) => {}) {
+        try {
+            const data = await axiosApiGetData(endpointUrl.getTopSelling(page));
             const products = data.map(res => {
                 const categoryRes = new category();
                 const productRes = new product(res);
                 productRes.response();
                 return productRes;
-            })
+            });
             responseData(products);
-        }).catch(err => {
+        } catch (err) {
             console.log(">>> err get data: ", err);
-        })
+        }
+    }
+
+    async getDetail(productId, responseData = (product) => {}) {
+        try {
+            const data = await axiosApiGetData(endpointUrl.getProductDetail(productId));
+            const productRes = new product(data);
+            productRes.response();
+            responseData(productRes);
+        } catch (err) {
+            console.log(">>> err get detail: ", err);
+        }
+    }
+
+    async addProduct(productData) {
+        try {
+            await axiosApiAddData(endpointUrl.addProduct(), productData);
+            console.log("finish");
+        } catch (err) {
+            console.log(">>> err add product: ", err);
+        }
     }
 }
--- src/view/components/elements/head/head.js
+++ src/view/components/elements/head/head.js
@@ -1,35 +1,32 @@
-import { elementHtml } from "@/utils"
+import { elementHtml } from "@/utils";
 import { ic_filter, ic_dots_vertical } from "@/constants";
-const elHtml = new elementHtml();
 
+const elHtml = new elementHtml();
 
 export const headSelling = () => {
     const container1 = elHtml.divELement("top-container_left-header");
-    const span1 = elHtml.spanElement("","Top Selling Product");
+    const span1 = elHtml.spanElement("", "Top Selling Product");
 
     const container2 = elHtml.divELement("");
-    const img = elHtml.imgElement(ic_filter,"icon","");
-    const span2 = elHtml.spanElement("","Filters");
+    const img = elHtml.imgElement(ic_filter, "icon", "");
+    const span2 = elHtml.spanElement("", "Filters");
 
-    container2.appendChild(img);
-    container2.appendChild(span2);
-    container1.appendChild(span1);
-    container1.appendChild(container2);
+    container2.append(img, span2);
+    container1.append(span1, container2);
 
-    return container1.outerHTML;
-}
+    return container1;
+};
 
-export const headSaleLocation = () =>{
+export const headSaleLocation = () => {
     const container1 = elHtml.divELement("top-container_right-header");
-    const container2 = elHtml.divELement("");
-    const span1 = elHtml.spanElement("","Sales by Location");
-    const span2 = elHtml.spanElement("","Sales performance by location");
-    const image1 = elHtml.imgElement(ic_dots_vertical,"icon","");
 
-    container2.appendChild(span1);
-    container2.appendChild(span2);
-    container1.appendChild(container2);
-    container1.appendChild(image1);
-    
-    return container1.outerHTML;
-}
+    const container2 = elHtml.divELement("");
+    const span1 = elHtml.spanElement("", "Sales by Location");
+    const span2 = elHtml.spanElement("", "Sales performance by location");
+    const image1 = elHtml.imgElement(ic_dots_vertical, "icon", "");
+
+    container2.append(span1, span2);
+    container1.append(container2, image1);
+
+    return container1;
+};
