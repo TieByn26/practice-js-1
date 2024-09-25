503fd895343d781838a433f325e18b16524c5a2d
TieByn26 <dxuantienn@gmail.com>
2024-09-25 23:50:20 +0700

    recent order #1

--- src/view/components/elements/foot/index.js
+++ src/view/components/elements/foot/index.js
@@ -1,1 +1,2 @@
 export * from "./foot";
+export * from "./footRecent";
---
+++ src/view/components/elements/head/headRecenOrder.js
@@ -0,0 +1,29 @@
+import { elementHtml } from "@/utils";
+import { ic_filter } from "@/constants";
+import { button } from "../button";
+const element = new elementHtml();
+const createButton = new button();
+export class HeadRecentOrder {
+    constructor() {
+    }
+    static headContent() {
+        const container = element.divELement("head-recent-order");
+        const containerLeft = element.divELement("head-recent-order_left");
+        const spanTitle = element.spanElement("span-title", "Recent Orders");
+        const spanOrder = element.spanElement("span-order", "+2 Orders");
+        containerLeft.append(spanTitle, spanOrder);
+
+        const containerMidle = element.divELement("head-recent-order_middle");
+        const imgFilter = element.imgElement(ic_filter,"icon","");
+        const spanFilter = element.spanElement("","Filters");
+        containerMidle.append(imgFilter, spanFilter);
+
+        const buttonSeeMore = createButton.render("button-blue",{to: '/404', label : 'See More',icon: ""});
+
+        container.append(containerLeft, containerMidle, buttonSeeMore);
+        return container;
+    }
+    static render() {
+        return this.headContent();
+    }
+}
